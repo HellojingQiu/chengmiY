c991eb434df58d5b21df00501e6d020dfbd20bde
qianfeng <qianfeng@aochangzhang.local>
2015-01-20 12:40:36 +0800

    Initial Commit

---
+++ chengmi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chengmi
+//
+//  Created by qianfeng on 15/1/20.
+//  Copyright (c) 2015年 yxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
